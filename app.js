069af7b38e143658f67a554fdaa08fce02200a7a
ysc <yuansuncheng@qq.com>
2019-05-29 16:17:18 +0800

    添加了 /t 路由

--- app.js
+++ app.js
@@ -4,3 +4,4 @@ app.listen(3000);
 app.get('/', (req,res)=>{res.send('ok')});
 app.get('/demo', (req,res)=>{res.send('Hello Demo')});
 app.get('/world', (req,res)=>{res.send('Hello World')});
+app.post('/t',(req,res)=> {res.send('Hello T')});
